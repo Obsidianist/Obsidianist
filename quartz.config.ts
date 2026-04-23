577b6e1f0dc2c516b2df832528b024220b00ff93
Obsidianist <Obsidianist0@gmail.com>
2026-04-23 23:10:08 +0800

    Quartz sync: Apr 23, 2026, 11:10 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "obsidianist.obsidianistO.workers.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
